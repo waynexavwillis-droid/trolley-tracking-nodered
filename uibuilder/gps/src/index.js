8942ee2bc105494de0f029c3f70dba59a90142f9
Wayne <waynexavwillis@gmail.com>
2025-11-12 13:53:22 +0800

    commit test

---
+++ uibuilder/gps/src/index.js
@@ -0,0 +1,11 @@
+// Give VS Code IntelliSense for uibuilder
+/// <reference path="../types/uibuilder.d.ts" />
+
+/** The simplest use of uibuilder client library
+ * See the docs if the client doesn't start on its own.
+ */
+
+// Listen for incoming messages from Node-RED and action
+// uibuilder.onChange('msg', (msg) => {
+//     // do stuff with the incoming msg
+// })
